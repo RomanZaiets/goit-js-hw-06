c40020dc9e962e0306346abbad568a29d0bedf7d
Roman Zaiets <acercg3@gmail.com>
2023-04-22 21:07:08 +0300

    add js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientListEl = document.querySelector("#ingredients");
+const makeNameIngredients = (content) => {
+  return content.map((ingredient) => {
+    const ingrItemEl = document.createElement("li");
+    ingrItemEl.textContent = ingredient;
+    ingrItemEl.classList.add("item");
+    return ingrItemEl;
+  });
+};
+const elements = makeNameIngredients(ingredients);
+ingredientListEl.append(...elements);
+console.log(makeNameIngredients);
